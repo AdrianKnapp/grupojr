2e8128e66a92fc2857efec7ae312f08e6bd33bfa
AdrianKnapp <adrknapp@gmail.com>
2022-07-19 12:02:09 -0300

    Added /sobre-nos and /transportadora text

--- src/components/PagesList/PageItem/PageDetails.tsx
+++ src/components/PagesList/PageItem/PageDetails.tsx
@@ -1,7 +1,6 @@
 import { Flex, Text } from '@chakra-ui/react';
 import { BiMap } from 'react-icons/bi';
 import { theme } from '../../../styles/theme';
-import useMediaQuery from '../../../hooks/useMediaQuery';
 
 type PageDetailsProps = {
   name: string;
@@ -9,16 +8,6 @@ type PageDetailsProps = {
 }
 
 export function PageDetails({ name, local }: PageDetailsProps) {
-  const [isMobile] = useMediaQuery('(max-width: 950px)');
-
-  const getLocal = () => {
-    if (isMobile) {
-      return local;
-    }
-
-    return local.length > 40 ? `${local.substring(0, 55)}...` : local;
-  };
-
   return (
     <Flex
       padding={[
@@ -45,7 +34,7 @@ export function PageDetails({ name, local }: PageDetailsProps) {
         <Flex gap={3}>
           <BiMap fontSize={35} color={theme.colors.text} />
           <Text fontWeight="medium" fontSize={[12, 14, 16]}>
-            {getLocal()}
+            {local}
           </Text>
         </Flex>
       )}
--- src/pages/transportadora.tsx
+++ src/pages/transportadora.tsx
@@ -20,9 +20,10 @@ export default function Transportadora({ images }: TransportadoraProps) {
       </Head>
       <InfosPage
         title="Transportadora"
-        description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
         bannerImages={images}
-      />
+      >
+        O Grupo JR é uma empresa focada em levar combustíveis de qualidade para seus clientes e, pensando exatamente nesse ponto, decidimos desenvolver nossa própria logística para agregar valor e reconhecimento ao nosso grupo! Com a logística própria, possuímos uma facilidade de ir e vir com maior segurança e com isso, sempre levamos o melhor combustível para você. Oferecemos rapidez, eficiência e segurança para todos os nossos clientes. Abasteça com a gente!
+      </InfosPage>
     </>
   );
 }
