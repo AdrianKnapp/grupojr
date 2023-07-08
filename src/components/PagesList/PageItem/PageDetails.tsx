677568b4bc31e070e443c62ef96e468aeeb0cfdb
AdrianKnapp <adrknapp@gmail.com>
2023-07-08 14:22:42 -0300

    feat: added see-more button on home page cards list

--- src/components/PagesList/PageItem/PageDetails.tsx
+++ src/components/PagesList/PageItem/PageDetails.tsx
@@ -1,4 +1,4 @@
-import { Flex, Text } from '@chakra-ui/react';
+import { Button, Flex, Text } from '@chakra-ui/react';
 import { BiMap } from 'react-icons/bi';
 import { theme } from '../../../styles/theme';
 
@@ -10,34 +10,47 @@ type PageDetailsProps = {
 export function PageDetails({ name, local }: PageDetailsProps) {
   return (
     <Flex
+      direction="column"
+      justifyContent="space-between"
       padding={[
         3,
         3,
         5,
       ]}
-      direction="column"
-      gap={[
-        1,
-        2,
-        3,
-      ]}
     >
-      <Text
-        as="h3"
-        color={theme.colors.text}
-        fontWeight="extrabold"
-        fontSize={[18, 22, 26]}
+      <Flex
+        direction="column"
+        gap={1}
       >
-        {name}
-      </Text>
-      {local && (
+        <Text
+          as="h3"
+          color={theme.colors.text}
+          fontWeight="extrabold"
+          fontSize={[18, 22, 26]}
+        >
+          {name}
+        </Text>
+        {local && (
         <Flex gap={3}>
           <BiMap fontSize={35} color={theme.colors.text} />
           <Text fontWeight="medium" fontSize={[12, 14, 16]}>
             {local}
           </Text>
         </Flex>
-      )}
+        )}
+      </Flex>
+      <Button
+        fontSize={[14]}
+        height="auto"
+        padding={[
+          3,
+          3,
+          3,
+        ]}
+        mt={[4, 0]}
+      >
+        Ver mais
+      </Button>
     </Flex>
   );
 }
