99888e8d8ee850213b13947723d2849993fbb1ce
AdrianKnapp <adrknapp@gmail.com>
2024-02-26 09:01:17 -0300

    feat: adjusted email

--- src/components/Footer/side/Right.tsx
+++ src/components/Footer/side/Right.tsx
@@ -6,14 +6,11 @@ import { TextInfo } from '../TextInfo';
 export function Right() {
   return (
     <Flex width="100%" height="100%" direction="column" justify="start">
-      <Header>
-        Contatos
-      </Header>
+      <Header>Contatos</Header>
+      <TextInfo>Telefone: (022) 3833-9170</TextInfo>
       <TextInfo>
-        Telefone: (022) 3833-9170
-      </TextInfo>
-      <TextInfo>
-        E-mail: escritoriobj.postos@hotmail.com
+        E-mail:
+        <a href="mailto:adm@grupojrpostos.com">adm@grupojrpostos.com</a>
       </TextInfo>
       <Spacer mt={5} />
       {/* <IconsBox /> */}
